fecb59c24b61d410eee5c8e01f9a3fc308214e3d
erictriestocode <eric.northrop@gmail.com>
2019-03-05 16:28:12 -0600

    database creation and schema modifications

--- clamazon_db.sql
+++ clamazon_db.sql
@@ -4,7 +4,7 @@ CREATE database bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   price DECIMAL(15,2) NULL,
