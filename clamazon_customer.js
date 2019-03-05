6e8092771e16d1eb23a34e2f24a1e8fe9dbdb97b
erictriestocode <eric.northrop@gmail.com>
2019-03-05 14:56:38 -0600

    Switched to localhost SQL server

--- clamazon_customer.js
+++ clamazon_customer.js
@@ -18,6 +18,22 @@ let mysql = require("mysql");
 
 // END HOSTED SERVER CONNECTION
 
+// LOCALHOST SERVER
+let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "eric",
+    password: "password",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("You have connected to the database. ID: " + connection.threadId + "\n");
+    mainProgram();
+});
+// END LOCALHOST SERVER
+
 function mainProgram(){
     console.log("Program works!");
 
