d64481ee1a783c666a3fefc5f2adfabf5847815a
Eric Northrop <eric.northrop@gmail.com>
2019-03-05 19:41:53 +0000

    Ok I give up, Going to localhost

--- clamazon_customer.js
+++ clamazon_customer.js
@@ -1,19 +1,22 @@
-const inquirer = require("inquirer");
-const mysql = require("mysql");
+let inquirer = require("inquirer");
+let mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: "server1.deprecatedhope.com",
-    port: 9119,
-    user: "customer",
-    password: "customerpassword",
-    database: "bamazon_db"
-});
+//HOSTED SERVER CONNECTION (NOT WORKING)
 
-connection.connect(function(err){
-    if (err) throw err;
-    console.log("You have connected to the database. ID: " + connection.threadId + "\n");
-});
+// let connection = mysql.createConnection({
+//     host: "server1.deprecatedhope.com",
+//     port: 3306,
+//     user: "customer",
+//     password: "customerpassword",
+//     database: "bamazon_db"
+// });
 
+// connection.connect(function(err){
+//     if (err) throw err;
+//     console.log("You have connected to the database. ID: " + connection.threadId + "\n");
+// });
+
+// END HOSTED SERVER CONNECTION
 
 function mainProgram(){
     console.log("Program works!");
